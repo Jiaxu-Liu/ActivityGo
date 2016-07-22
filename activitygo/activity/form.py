82aee5db1298043f6f2255bc6e28b1e7013fc4e4
liuzhenyang14 <liuzhenyang14@gmail.com>
2016-07-22 22:54:31 +0800

    modify form.py

--- activitygo/activity/form.py
+++ activitygo/activity/form.py
@@ -6,3 +6,10 @@ class UserForm(forms.Form):
 	password = forms.CharField(label = 'password', max_length = 100, widget=forms.PasswordInput())
 	email = forms.CharField(label='email',max_length = 100)
 	phone = forms.CharField(label='phone',max_length=100)
+
+class ChangePasswordForm(forms.Form):
+	oldpassword = forms.CharField(label = 'old password', max_length = 100, widget = forms.PasswordInput())
+	newpassword = forms.CharField(label = 'new password', max_length = 100, widget = forms.PasswordInput())
+	
+
+
