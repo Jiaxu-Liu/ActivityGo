75a07ccecddfb13378298772bfa27de548035a51
liuzhenyang14 <liuzhenyang14@gmail.com>
2016-07-26 16:57:30 +0800

    show more infos in admin

--- activitygo/activity/admin.py
+++ activitygo/activity/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from activity.models import User, Activities
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Activities)
+class UserAdmin(admin.ModelAdmin):
+	fields = ('username', 'credit')
+	list_display = ('username', 'phone', 'email', 'credit')
+
+class ActivitiesAdmin(admin.ModelAdmin):
+	fields = ('aname', 'adate', 'alocation', 'aorganiser', 'astatus')
+	list_display = ('aname', 'adate', 'alocation', 'aorganiser', 'astatus')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Activities, ActivitiesAdmin)
