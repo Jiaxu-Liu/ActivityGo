774e981705c65b59aaa902443f20f40931a1f572
liuzhenyang14 <liuzhenyang14@gmail.com>
2016-07-28 15:29:25 +0800

    water

--- activitygo/activity/admin.py
+++ activitygo/activity/admin.py
@@ -12,16 +12,20 @@ def change_status_to_1(self, request, queryset):
 def change_status_to_0(self, request, queryset):
 	rowupdate = queryset.update(astatus = 0)
 
+def change_status_to_2(self, request, queryset):
+	rowupdate = queryset.update(astatus = 2)
+
 
 class ActivitiesAdmin(admin.ModelAdmin):
 	fields = ('aname', 'adate', 'alocation', 'aorganiser','aparticipantnum', 'astatus')
 	list_display = ('aname', 'adate', 'alocation',  'aorganiser', 'astatus')
-	actions = [change_status_to_1, change_status_to_0]
+	actions = [change_status_to_1, change_status_to_0, change_status_to_2]
 
 
 
 change_status_to_0.short_description = "change selected activities status to 0"
 change_status_to_1.short_description = "change selected activities status to 1"
+change_status_to_2.short_description = "change selected activities status to 2"
 
 
 admin.site.register(User, UserAdmin)
